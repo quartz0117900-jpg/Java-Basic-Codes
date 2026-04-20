b6ada375f7b3ec02712e8bdd9c54b406c1bf8dab
5HR3Y_pixel <quartz0117900@gmail.com>
2026-04-20 18:05:15 +0530

    Question added

--- PropertyMain.java
+++ PropertyMain.java
@@ -1,3 +1,20 @@
+/*
+3. Declare an interface called Property containing a method computePrice to compute 
+and return the price. The interface is to be implemented by following two classes i) 
+Bungalow and ii) Flat. 
+Both the classes have following data members - name - constructionArea 
+ 
+The class Bungalow has an additional data member called landArea. Define 
+computePrice for both classes for computing total price. Use following rules for 
+computing total price by summing up sub-costs: 
+Construction cost(for both classes):Rs.500/- per sq.feet 
+Additional cost ( for Flat)     : Rs. 200000/- 
+( for Bungalow ): Rs. 200/- per sq. 
+feet for landArea 
+Land cost  ( only for Bungalow ): Rs. 400/- per sq. feet 
+Define method main to show usage of method computePrice. 
+
+*/
 // Interface - acts like a contract
 // any class that implements it MUST have computePrice()
 interface Property {
