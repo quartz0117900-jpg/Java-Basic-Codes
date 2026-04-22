f35fbbb24458647555ecb9bd8651ff8687cf243a
5HR3Y_pixel <quartz0117900@gmail.com>
2026-04-22 17:30:58 +0530

    GeometricShape

--- GeometricShapeTest.java
+++ GeometricShapeTest.java
@@ -1,4 +1,42 @@
-// Base interface - every geometric shape must have describe()
+/*
+Q.4)  
+iv) Define following classes and interfaces. 
+    public interface GeometricShape { 
+        public void describe(); 
+    } 
+    public interface TwoDShape extends GeometricShape { 
+        public double area(); 
+    } 
+    public interface ThreeDShape extends GeometricShape { 
+        public double volume(); 
+    } 
+    public class Cone implements ThreeDShape  { 
+        private double radius; 
+        private double height; 
+        public Cone (double radius, double height)  
+        public double volume()  
+        public void describe()  
+    } 
+    public class Rectangle implements TwoDShape  { 
+    
+    
+        private double width, height; 
+        public Rectangle (double width, double height)  
+        public double area()  
+        public double perimeter()  
+        public void describe() 
+    
+    } 
+    public class Sphere implements ThreeDShape  { 
+        private double radius; 
+        public Sphere (double radius)  
+        public double volume() 
+        public void describe()  
+    } 
+    Define test class to call various methods of Geometric Shape   
+    */
+
+    // Base interface - every geometric shape must have describe()
 interface GeometricShape {
     public void describe(); // no body - implementing class must define it
 }
