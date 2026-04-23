0b6c4fd9e855067ea8b13400203c6ede06d34fc3
5HR3Y_pixel <quartz0117900@gmail.com>
2026-04-23 22:12:45 +0530

    Q5 (1)

--- CPUMain.java
+++ CPUMain.java
@@ -1,3 +1,29 @@
+/*
+Q5.
+1) Inner Class: 
+    Define two nested classes: Processor and RAM inside the outer class: CPU with following 
+    data members 
+    class CPU { 
+        double price; 
+        class Processor{   //    nested class 
+            double cores; 
+            double catch() 
+            String manufacturer; 
+            double getCache()  
+            void displayProcesorDetail() 
+        } 
+        protected class RAM{ // nested protected class 
+            // members of protected nested class 
+            double memory; 
+            String manufacturer; 
+            Double clockSpeed; 
+            double getClockSpeed() 
+            void displayRAMDetail() 
+        } 
+    } 
+    1. Write appropriate Constructor and create instance of Outer and inner class and call the 
+    methods in main function 
+    */
 // Outer class CPU
 class CPU {
 
